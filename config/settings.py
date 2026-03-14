be7efd79cb7d21b469532eaccad4154e6895aafc
Bryan <sidustarop@gmail.com>
2026-03-14 00:47:54 -0400

    Configurar base de datos para producción en Azure y actualizar requisitos

--- config/settings.py
+++ config/settings.py
@@ -106,12 +106,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if IS_PRODUCTION:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'mssql',
+            'NAME': os.environ.get('AZURE_SQL_DB'),
+            'HOST': os.environ.get('AZURE_SQL_SERVER'),
+            'PORT': '1433',
+            'OPTIONS': {
+                'driver': 'ODBC Driver 18 for SQL Server',
+                'extra_params': 'Authentication=ActiveDirectoryDefault',
+            },
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
